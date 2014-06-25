39a7d6c1f77dd6e3f777274f21be9d18ac2fed28
Arthur Souviron <arthur.souviron@leikir-studio.com>
2014-06-25 17:36:35 +0100

    add token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,24 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   acts_as_token_authentication_handler_for User, fallback_to_devise: false
-
+  before_filter :cors_set_access_control_headers
 
   #protect_from_forgery with: :exception
-  protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
+  #protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Max-Age'] = "1728000"
+  end
+
+
+
+  # def cor
+  #   headers["Access-Control-Allow-Origin"] = "*"
+  #   headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE OPTIONS}.join(",")
+  #   headers["Access-Control-Allow-Headers"] = %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
+  #   head(:ok) if request.request_method == "OPTIONS"
+  # end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Api::Application.routes.draw do
 
+
+  #match '*all' => 'application#cor', :constraints => {:method => 'OPTIONS'}
+
   devise_for :users, :controllers => { :registrations => 'registrations', :sessions => 'sessions' }
   
 
