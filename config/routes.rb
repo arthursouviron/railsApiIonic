bcbc5b952244337a6830a020c9975efdb9072a97
Arthur Souviron <arthur.souviron@leikir-studio.com>
2014-06-26 11:56:45 +0100

    ca marche?

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,14 @@ Api::Application.routes.draw do
   #match '*all' => 'application#cor', :constraints => {:method => 'OPTIONS'}
 
 
+ 
 
   devise_for :users, :controllers => { :registrations => 'registrations', :sessions => 'sessions' }
   
+  devise_scope :user do
+    match '/users/sign_in', to: 'sessions#create', via: [:options]
+  end
+
 
   resources :users do
     resources :contacts
