9727bd6eaa224fff8102367c54235b85c5d4ffa7
Arthur Souviron <arthur.souviron@leikir-studio.com>
2014-06-26 10:05:58 +0100

    remove bundle

--- vendor/bundle/ruby/2.1.0/gems/angularjs-rails-1.2.18/lib/angularjs-rails/version.rb
+++
@@ -1,6 +0,0 @@
-module AngularJS
-  module Rails
-    VERSION = "1.2.18"
-    UNSTABLE_VERSION = "1.3.0-beta.13"
-  end
-end
--- vendor/bundle/ruby/2.1.0/gems/simple_token_authentication-1.5.0/lib/simple_token_authentication.rb
+++
@@ -1,7 +0,0 @@
-require 'simple_token_authentication/acts_as_token_authenticatable'
-require 'simple_token_authentication/acts_as_token_authentication_handler'
-require 'simple_token_authentication/configuration'
-
-module SimpleTokenAuthentication
-  extend Configuration
-end
