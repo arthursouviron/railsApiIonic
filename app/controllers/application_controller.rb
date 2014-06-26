79f96d306719aeb59593d1c4a6428a3ebff7d321
Arthur Souviron <arthur.souviron@leikir-studio.com>
2014-06-26 10:02:20 +0100

    test header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,15 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   acts_as_token_authentication_handler_for User, fallback_to_devise: false
-  before_filter :cors_set_access_control_headers
+  after_filter :set_access_control_headers
 
   #protect_from_forgery with: :exception
   #protect_from_forgery with: :null_session, :if => Proc.new { |c| c.request.format == 'application/json' }
 
-  def cors_set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = '*'
-    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-    headers['Access-Control-Max-Age'] = "1728000"
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*' 
+    headers['Access-Control-Request-Method'] = '*' 
   end
 
 
